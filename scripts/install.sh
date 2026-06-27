3cc7c882c4d572182fcc8415f8be534f76e6fca7
hayateNIX <297493641+hayateNIX@users.noreply.github.com>
2026-06-27 23:24:09 +0200

    Create project base structure

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "=================================="
+echo "      Hayate OS Installer"
+echo "=================================="
+
+echo "Instalando paquetes base..."
+
+echo "Instalación completada."
